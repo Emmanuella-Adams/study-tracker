657a235537b9ce0ac8cff18c088abd01ddfb9ef4
Emmanuella Adams <ellaadams206@gmail.com>
2025-11-25 20:12:14 +0100

    updated the tracker.py file with more functions

--- tracker.py
+++ tracker.py
@@ -44,4 +44,65 @@ def save_logs(logs):
         print('logs saved successfully!')
 
     except Exception as e:
-        print(f'Error saving logs {e}')
+        print(f"❌ Error saving logs: {e}")
+
+
+        
+def validate_number(prompt):
+    """
+    Prompts the user and ensures the input is a positive integer.
+    """
+    while True:
+        try:
+            value = input(prompt)
+            num = int(value)
+            if num > 0:
+                return num
+            else:
+                print("❌ Duration/Input must be a positive number.")
+        except ValueError:
+            print("❌ Invalid input. Please enter a whole number.")
+
+
+#we then create a function that calls on the current date
+
+from datetime import datetime
+
+def get_current_date():
+    
+    return datetime.now().strftime('%y-%m-%d')
+
+#we then implement the core infrastructure
+
+def add_study_session():
+
+    #load existing data
+    logs = extract_logs()
+    print('\n ADD NEW STUDY SESSION')
+
+    #prompt for input
+    subject = input('subjects studied: ').strip
+    duration = validate_number('duration in minutes, please input a positive integer: ')
+    notes = input('Inputes notes taken (optional)').strip
+
+    #get the current date for the log entry
+    date = get_current_date()
+
+    #create new session dictionary
+
+    new_session = {
+
+        'subject': subject,
+        'duration' : duration,
+        'notes' : notes,
+        'date' : date
+    }
+
+    #to append the new session
+    logs.append(new_session)
+
+
+    #to save the logs
+    save_logs(logs)
+
+    print('new session successfully updated')
